a5e3b739e6b1c9b87f63c05869c77e2ecb7e130a
IgorYavorskiy <thorvin.mail@gmail.com>
2019-10-09 16:46:16 +0300

    Работа над объектом контроллером

--- battleship.js
+++ battleship.js
@@ -49,8 +49,36 @@ var model = {
             }
         }
         return true;
+    },
+};
+
+var controller = {
+    guesses: 0,
+
+    proceddGuess: function(guess){
+        
     }
 };
 
+function parseGuess(guess){
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    if (guess === null || guess.length != 2) {
+        alert("Oops, please enter a letter and a number on the board");
+    } else {
+        firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >=board.boardSize){
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+
+console.log(parseGuess("A0"));
 
 

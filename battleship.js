054d0829ae7a717b9249b6cc459bb93f96b75e76
IgorYavorskiy <thorvin.mail@gmail.com>
2019-10-09 08:45:01 +0300

    Удалил проверку из объекта вывода

--- battleship.js
+++ battleship.js
@@ -13,16 +13,6 @@ var view = {
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-
-
-view.displayMessage("HEllo!");
-
 
 
 
